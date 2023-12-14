9418266a41b555761792c2b13d19fd1d81d52486
hamiddashti <ahangar.hamid@gmail.com>
2023-12-14 11:20:14 -0600

    Rerpojection

--- src/test.py
+++ src/test.py
@@ -3,88 +3,190 @@ import numpy as np
 import pandas as pd
 from osgeo import gdal, osr
 import h5py
+import rasterio
 
-root = "/home/hamid/dscovr/"
-file = "data/DSCOVR_EPIC_L2_VESDR_02_20160401025239_03.h5"
-h5fpath  = os.path.join(root, file)   
-h5f = h5py.File(h5fpath, "r") 
-date = file.split("_")[5]
+# import matplotlib.pylab as plt
 
-xyPath  = root+ "MCDLCHKM.V2010_01.REGIONALbio.10018m.BlocksOP.h5"
-xyf = h5py.File(xyPath, "r") 
+# import xarray as xr
+# import rioxarray
 
 
-outpath = r"/home/hamid/dscovr/"
+def qa_num_to_binary(qa_num):
+    # Return True if good vegetation data else False
+    # Refer to the DSCOVR manual for the QA codes
+    if np.isnan(qa_num):
+        return False
+    else:
+        binary_string = bin(int(qa_num) & 0xFFFF)[2:].zfill(16)
+        if np.all(np.array(list(binary_string)[-13:]) == "0"):
+            return True
+        return False
 
-field = '19_MAIAC_CloudLWmask'
-tile = "tile20"
+        # return binary_string
 
 
-# Get geolocations from land cover map and create a vrt map
-xyf_x = np.array(xyf[tile]['Geolocation']['Longitude']).reshape(-1,1)
-xyf_y = np.array(xyf[tile]['Geolocation']['Latitude']).reshape(-1,1)
-h5f_t = np.array(h5f[tile][field]).reshape(-1,1)
+vectorized_qa_num_to_binary = np.vectorize(qa_num_to_binary)
 
-out = np.hstack((xyf_x, xyf_y, h5f_t))
-out = out.astype(float)
-df = pd.DataFrame(out, columns = ['Lon','Lat','data'])
 
-df = df[df['Lon'] > -999.0]
-df = df[df['Lat'] > -999.0]
-df.data[df.data <= -9998] = -9999.0
+def filter_qa(h5f, tile, field):
+    # Filter data based on the quality
+    var = np.array(h5f[tile][field])
+    var = var.astype(float)
+    var[(var >= -9999) & (var <= -9997)] = np.nan
+    qa = np.array(h5f[tile]["06_QA_VESDR"])
+    qa = np.where(np.isfinite(var), qa, np.nan)
+    good_flag = vectorized_qa_num_to_binary(qa)
+    if field == "12_ERTI":
+        Scale_factor = 0.01
+    elif (
+        (field == "13_W443")
+        | (field == "14_W551")
+        | (field == "15_W680")
+        | (field == "16_W780")
+        | (field == "07_SZA")
+        | (field == "08_VZA")
+        | (field == "09_SAA")
+        | (field == "10_VAA")
+    ):
+        Scale_factor = 1
+    else:
+        # Scale factor for LAI,SLAI,FPAR,NDVI,DASF
+        Scale_factor = 0.001
 
-df.to_csv(os.path.join(outpath, "{0}_{1}_{2}.csv".format(tile, field, date)),sep=",",index=False)
-msg = '''<OGRVRTDataSource>
-    <OGRVRTLayer name="{0}_{1}_{2}">
-        <SrcDataSource>{3}_{4}_{5}.csv</SrcDataSource>
-        <GeometryType>wkbPoint</GeometryType>
-        <GeometryField encoding="PointFromColumns" x="Lon" y="Lat" z="data"/>
-    </OGRVRTLayer>
-</OGRVRTDataSource>'''.format(tile, field, date, tile, field, date)
-        
-msgpath = os.path.join(outpath, "{0}_{1}_{2}.vrt".format(tile, field, date))
-file = open(msgpath, 'w')
-file.write(msg)   
-file.close()  
+    var_good = np.where(good_flag == True, var, np.nan) * Scale_factor
+    return var_good
 
-if (tile=='tile00'):
-    xmin, xmax = -25,65
-    ymin,ymax = 0,90
-if (tile=='tile01'):
-    xmin, xmax = -25,65
-    ymin,ymax = -90,0
 
-if (tile=='tile10'):
-    xmin, xmax = 65,155
-    ymin,ymax = 0,90
+def make_vrt(outpath, xyPath, tile, var_good, field, date):
+    # xypath only has the geometric info so we can use it for
+    xyf = h5py.File(xyPath, "r")
+    # Get geolocations from land cover map and create a vrt map
+    xyf_x = np.array(xyf[tile]["Geolocation"]["Longitude"]).reshape(-1, 1)
+    xyf_y = np.array(xyf[tile]["Geolocation"]["Latitude"]).reshape(-1, 1)
+    # h5f_t = np.array(h5f[tile][field]).reshape(-1,1)
+    h5f_t = var_good.reshape(-1, 1)
 
-if (tile=='tile11'):
-    xmin, xmax = 65,155
-    ymin,ymax = -90,0
+    out = np.hstack((xyf_x, xyf_y, h5f_t))
+    out = out.astype(float)
+    df = pd.DataFrame(out, columns=["Lon", "Lat", "data"])
+    df = df[df["Lon"] > -999.0]
+    df = df[df["Lat"] > -999.0]
+    df.data[df.data <= -9998] = -9999.0
 
-if (tile=='tile20'):
-    xmin, xmax = -115,155
-    ymin,ymax = 0,90
+    df.to_csv(
+        os.path.join(outpath, "{0}_{1}_{2}.csv".format(tile, field, date)),
+        sep=",",
+        index=False,
+    )
+    msg = """<OGRVRTDataSource>
+        <OGRVRTLayer name="{1}_{2}_{3}">
+            <SrcDataSource>{0}{1}_{2}_{3}.csv</SrcDataSource>
+            <GeometryType>wkbPoint</GeometryType>
+            <GeometryField encoding="PointFromColumns" x="Lon" y="Lat" z="data"/>
+        </OGRVRTLayer>
+    </OGRVRTDataSource>""".format(
+        outpath, tile, field, date
+    )
 
-if (tile=='tile21'):
-    xmin, xmax = -115,155
-    ymin,ymax = -90,0
+    msgpath = os.path.join(outpath, "{0}_{1}_{2}.vrt".format(tile, field, date))
+    file = open(msgpath, "w")
+    file.write(msg)
+    file.close()
 
-if (tile=='tile30'):
-    xmin, xmax = -115,-25
-    ymin,ymax = 0,90
 
-if tile == 'tile31':
-    xmin, xmax = -115,-25
-    ymin,ymax = -90,0
+def reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax):
+    scmd = (
+        "gdal_grid -a linear:radius=0:nodata=-9999.0 -txe %f %f -tye %f %f -tr 0.1 0.1 -a_srs EPSG:4326 -of GTiff -ot Float64 -l %s %s %s"
+        % (xmin, xmax, ymin, ymax, inputfile1, inputfile2, outputfile)
+    )
+    os.system(scmd)
 
-inputfile1 = "{0}_{1}_{2}".format(tile, field, date)
-inputfile2 = "{0}_{1}_{2}.vrt".format(tile, field, date)
-outputfile = "{0}_{1}_{2}_{3}.tif".format(field, date, tile,"155_180") 
-outputfile = outputfile
-# scmd = 'gdal_grid -a linear:radius=0:nodata=-9999.0 -txe %f %f -tye %f %f -tr 0.1 0.1 -a_srs EPSG:4326 -of GTiff -ot Float64 -l %s %s %s'\
-#         % (xmin, xmax,ymin,ymax, inputfile1, inputfile2, outputfile)
-xmin, xmax = 155,180   # Swap the values
-scmd = 'gdal_grid -a linear:radius=0:nodata=-9999.0 -txe %f %f -tye %f %f -tr 0.1 0.1 -a_srs EPSG:4326 -of GTiff -ot Float64 -l %s %s %s' \
-       % (xmin, xmax, ymin, ymax, inputfile1, inputfile2, outputfile)
-os.system(scmd)
+
+def reproject(tile, field, date, outpath):
+    if tile == "tile00":
+        xmin, xmax = -25, 65
+        ymin, ymax = 0, 90
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + ".tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        os.remove(outpath + inputfile1 + ".csv")
+        os.remove(inputfile2)
+    elif tile == "tile01":
+        xmin, xmax = -25, 65
+        ymin, ymax = -90, 0
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + ".tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        os.remove(inputfile1 + ".csv")
+        os.remove(inputfile2)
+    elif tile == "tile10":
+        xmin, xmax = 65, 155
+        ymin, ymax = 0, 90
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + ".tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        os.remove(outpath + inputfile1 + ".csv")
+        os.remove(inputfile2)
+    elif tile == "tile11":
+        xmin, xmax = 65, 155
+        ymin, ymax = -90, 0
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + ".tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        os.remove(outpath + inputfile1 + ".csv")
+        os.remove(inputfile2)
+    elif tile == "tile30":
+        xmin, xmax = -115, -25
+        ymin, ymax = 0, 90
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + ".tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        os.remove(outpath + inputfile1 + ".csv")
+        os.remove(inputfile2)
+    elif tile == "tile31":
+        xmin, xmax = -115, -25
+        ymin, ymax = -90, 0
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + ".tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        os.remove(outpath + inputfile1 + ".csv")
+        os.remove(inputfile2)
+    elif tile == "tile20":
+        # Do the US side
+        xmin, xmax = -180, -115
+        ymin, ymax = 0, 90
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + "_US.tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        # Do the Asia side
+        xmin, xmax = 155, 180
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + "_Asia.tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        os.remove(outpath + inputfile1 + ".csv")
+        os.remove(inputfile2)
+    elif tile == "tile21":
+        # Do the US side
+        xmin, xmax = -180, -115
+        ymin, ymax = -90, 0
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + "_US.tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+
+        # Do the Asia side
+        xmin, xmax = 155, 180
+        inputfile1 = tile + "_" + field + "_" + date
+        inputfile2 = outpath + tile + "_" + field + "_" + date + ".vrt"
+        outputfile = outpath + field + "_" + date + "_" + tile + "_Asia.tif"
+        reproject_fun(inputfile1, inputfile2, outputfile, xmin, xmax, ymin, ymax)
+        os.remove(outpath + inputfile1 + ".csv")
+        os.remove(inputfile2)
